a26d0d803ed20a4bed2224a14a7bae06782b5e59
Nwankwo Chidera <nwankwochidera.david@gmail.com>
2024-02-21 13:37:38 +0100

    Applying Socket.io to it

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 //Terminate
 let terminate = false;
 
-
 //Import all neccessary routes
 const football_routes = require("./routes/sports/football");
 
+// function start() {
+//   terminate = false;
+//   const intervalId = setInterval(async () => {
+//     await live();
+//   }, 60000); // 60000 milliseconds = 1 minute
+//   // Clear the interval after a certain time (optional)
 
-function start() {
-  terminate = false;
-  const intervalId = setInterval(async () => {
-    await live();
-  }, 60000); // 60000 milliseconds = 1 minute
-  // Clear the interval after a certain time (optional)
+//   setTimeout(() => {
+//     if (terminate) {
+//       clearInterval(intervalId);
+//     }
+//   }, 1800000); // Clear after 5 minutes
+// }
 
-  setTimeout(() => {
-    if (terminate) {
-      clearInterval(intervalId);
-    }
-  }, 1800000); // Clear after 5 minutes
-}
-
-function disrupt() {
-  terminate = true;
-}
+// function disrupt() {
+//   terminate = true;
+// }
 
 app.use(cors());
-app.use("/api", football_routes);
 
-app.get("*", (req, res) => {
-  res.status(401).end("Invalid API call");
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
+// app.use("/api", football_routes);
 
-app.listen(PORT, () => {
-  console.log("Server Listening at PORT:", PORT);
+// app.get("*", (req, res) => {
+//   res.status(401).end("Invalid API call");
+// });
+
+io.on("connection", socket => {
+  console.log(`User Connected: ${socket.id}`);
+});
+
+server.listen(PORT, () => {
+  console.log("Server (SOCKET.IO) Listening at PORT:", PORT);
 });
