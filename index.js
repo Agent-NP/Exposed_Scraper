f58c249a649001d732f2dffb1df75f9a83366e29
Nwankwo Chidera <nwankwochidera.david@gmail.com>
2024-02-22 19:21:06 +0100

    I'm able to emit the event

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const { Server } = require("socket.io");
 const PORT = process.env.PORT || 3001;
 const football_routes = require("./routes/sports/football");
 const { live_football } = require("./controllers/sports/live");
+// const { yesterday_football } = require("./controllers/sports/yesterday");
 
 const app = express();
+
 let previous = [];
 
 // Function to fetch and broadcast live football data
 async function updateAndBroadcast() {
   const current = await live_football();
+  // const current = await yesterday_football();
   if (JSON.stringify(previous) !== JSON.stringify(current)) {
     previous = current;
+    // console.log(current);
     io.emit("live_football_update", current);
   }
 }
@@ -24,11 +28,17 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"],
-  },
+    methods: ["GET", "POST"]
+  }
 });
 
-const intervalId = setInterval(updateAndBroadcast, 60000); // Fetch every minute
+// Call the function immediately to emit initial data
+updateAndBroadcast();
+
+const intervalId = setInterval(updateAndBroadcast, 60000);
+
+// const intervalId = setInterval(updateAndBroadcast, 2000); // Fetch every minute
+
 setTimeout(() => clearInterval(intervalId), 1800000); // Clear after 5 minutes
 
 // API routes
@@ -40,7 +50,7 @@ app.get("*", (req, res) => {
 });
 
 // Socket.io connection handling
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log(`User Connected: ${socket.id}`);
 });
 
